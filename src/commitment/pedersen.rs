2741e0005a18fea90357a5b02eef6bb6db78abac
lrusso96 <russo.1699981@studenti.uniroma1.it>
2020-06-26 16:37:05 +0200

    Commitment schemes: refactoring for Pedersen and its implementation over Multiplicative Group

--- src/commitment/pedersen.rs
+++ src/commitment/pedersen.rs
@@ -1,85 +1,35 @@
-use crate::utils::gen_random;
-use openssl::bn::{BigNum, BigNumContext};
+use crate::group::{DDHGroup, Element};
 use openssl::error::ErrorStack;
-use std::fmt;
+use std::fmt::Debug;
+
+pub mod mult;
+
+pub type CommitterMult = mult::Committer;
+pub type CommMult = mult::Comm;
 
 /// A Pedersen Committer is represented here
-pub struct PedersenCommitter {
-    /// a temporary storage for BigNums
-    ctx: BigNumContext,
-    /// the generator of the group
-    g: BigNum,
-    /// the order of the group
-    q: BigNum,
-    /// a random (public) element of the group
-    h: BigNum,
+#[derive(Debug)]
+pub struct Committer<E: Element, G: DDHGroup<E>> {
+    group: G,
+    h: E,
 }
 
-impl fmt::Debug for PedersenCommitter {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("PedersenCommitter")
-            .field("g", &self.g)
-            .field("q", &self.q)
-            .field("h", &self.h)
-            .finish()
-    }
-}
+pub type Comm<E> = E;
 
-impl PedersenCommitter {
-    /// Generates a new instance of Pedersen Committer.
-    ///
-    /// # Parameters
-    ///
-    /// * `secpar`: The number of bits for prime number generation.
-    ///
-    /// # Examples
-    ///
-    ///```
-    ///use smc::commitment::{Committer, PedersenCommitter};
-    ///use openssl::bn::BigNum;
-    ///fn main() {
-    ///    println!("Hello, let's try this Pedersen commit!");
-    ///    let sec = 32;
-    ///    println!("I'm gonna use {} bits security", sec);
-    ///    let mut commiter = PedersenCommitter::new(sec).unwrap();
-    ///    println!("{:#?}", commiter);
-    ///    let msg = BigNum::from_u32(100).unwrap();
-    ///    print!("The commit for {} is: ", msg);
-    ///    let ret = commiter.commit(msg).unwrap();
-    ///    println!("{}", ret);
-    ///}
-    /// ```
-    #[allow(dead_code)]
-    pub fn new(secpar: i32) -> Result<PedersenCommitter, ErrorStack> {
-        // create context to manage the bignum
-        let ctx = BigNumContext::new()?;
-        // generate prime safe number q = 2p + 1
-        let mut q = BigNum::new()?;
-        q.generate_prime(secpar, true, None, None)?;
-        // generate random g (generator)
-        let g = gen_random(&q)?;
-        // generate random element h
-        let h = gen_random(&q)?;
-        Ok(Self { ctx, g, q, h })
-    }
-}
+impl<E: Element> super::Comm for Comm<E> {}
 
-impl super::Committer for PedersenCommitter {
+impl<E: Element + super::Value, G: DDHGroup<E>> super::Committer<Comm<E>, E> for Committer<E, G> {
     /// Generates a commit on a given message.
     ///
     /// # Parameters
     ///
     /// * `msg`: The message.
-    fn commit(&mut self, msg: BigNum) -> Result<BigNum, ErrorStack> {
+    fn commit(&mut self, msg: E) -> Result<Comm<E>, ErrorStack> {
         //x1 = g^r mod q
-        let r = gen_random(&self.q)?;
-        let mut x1 = BigNum::new()?;
-        x1.mod_exp(&self.g, &r, &self.q, &mut self.ctx)?;
-        //x1 = h^m mod q
-        let mut x2 = BigNum::new()?;
-        x2.mod_exp(&self.h, &msg, &self.q, &mut self.ctx)?;
-        let mut ret = BigNum::new()?;
-        ret.mod_mul(&x1, &x2, &self.q, &mut self.ctx)?;
-        Ok(ret)
+        let r = self.group.generate_random();
+        let x1 = self.group.pow(&r);
+        //x2 = h^m mod q
+        let x2 = self.group.exponentiate(&self.h, &msg);
+        Ok(self.group.multiply(&x1, &x2))
     }
 }
---
+++ src/commitment/pedersen/mult.rs
@@ -0,0 +1,38 @@
+use crate::group::{DDHGroup, MultGroup};
+use openssl::bn::BigNum;
+use openssl::error::ErrorStack;
+
+pub type Committer = super::Committer<BigNum, MultGroup>;
+pub type Comm = super::Comm<BigNum>;
+
+impl Committer {
+    /// Generates a new instance of Pedersen Committer.
+    ///
+    /// # Parameters
+    ///
+    /// * `secpar`: The number of bits for prime number generation.
+    ///
+    /// # Examples
+    ///
+    ///```
+    ///use smc::commitment::{Committer, PedersenCommitterMult};
+    ///use openssl::bn::BigNum;
+    ///fn main() {
+    ///    println!("Hello, let's try this Pedersen commit!");
+    ///    let sec = 32;
+    ///    println!("I'm gonna use {} bits security", sec);
+    ///    let mut commiter = PedersenCommitterMult::new(sec).unwrap();
+    ///    println!("{:#?}", commiter);
+    ///    let msg = BigNum::from_u32(100).unwrap();
+    ///    print!("The commit for {} is: ", msg);
+    ///    let ret = commiter.commit(msg).unwrap();
+    ///    println!("{}", ret);
+    ///}
+    /// ```
+    #[allow(dead_code)]
+    pub fn new(secpar: i32) -> Result<Committer, ErrorStack> {
+        let group = MultGroup::new(secpar)?;
+        let h = group.generate_random();
+        Ok(Self { group, h })
+    }
+}
