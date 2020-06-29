f7f9aaf639ee82a26daf1c46b028317706b97025
lrusso96 <russo.1699981@studenti.uniroma1.it>
2020-06-29 10:37:58 +0200

    ElGamal scheme: support for Elliptic Curves (#1)

--- src/commitment.rs
+++ src/commitment.rs
@@ -6,8 +6,12 @@ pub mod pedersen;
 
 pub type ElGamalCommitMult = elgamal::CommitMult;
 pub type ElGamalCommitterMult = elgamal::CommitterMult;
+pub type ElGamalCommitEc = elgamal::CommitEc;
+pub type ElGamalCommitterEc = elgamal::CommitterEc;
 pub type PedersenCommitMult = pedersen::CommitMult;
 pub type PedersenCommitterMult = pedersen::CommitterMult;
+pub type PedersenCommitEc = pedersen::CommitEc;
+pub type PedersenCommitterEc = pedersen::CommitterEc;
 
 pub trait Message {}
 pub trait Commit {}
@@ -15,23 +19,26 @@ pub trait Opening {}
 
 impl Message for BigNum {}
 
-pub trait Committer<M, C, O>
+pub trait Committer<C, O>
 where
-    M: Message,
     C: Commit,
     O: Opening,
 {
-    fn commit(&mut self, msg: M) -> Result<(C, O), ErrorStack>;
+    fn commit(&mut self, msg: BigNum) -> Result<(C, O), ErrorStack>;
     fn verify(&mut self, c: C, o: O) -> Result<bool, ErrorStack>;
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::{Committer, ElGamalCommitterMult, PedersenCommitterMult};
+    use super::{
+        Committer, ElGamalCommitterEc, ElGamalCommitterMult, PedersenCommitterEc,
+        PedersenCommitterMult,
+    };
     use openssl::bn::BigNum;
+
     #[test]
-    fn test_pedersen() {
+    fn test_pedersen_mult() {
         println!("Hello, let's try this Pedersen commit!");
         let sec = 32;
         println!("I'm gonna use {} bits security", sec);
@@ -45,7 +52,21 @@ mod tests {
     }
 
     #[test]
-    fn test_elgamal() {
+    fn test_pedersen_ec() {
+        println!("Hello, let's try this Pedersen commit!");
+        let sec = 32;
+        println!("I'm gonna use {} bits security", sec);
+        let mut commiter = PedersenCommitterEc::new(sec).unwrap();
+        //println!("{:#?}", commiter);
+        let msg = BigNum::from_u32(123).unwrap();
+        print!("The commit for {} is: ", msg);
+        let (c, o) = commiter.commit(msg).unwrap();
+        //println!("{}", c);
+        assert_eq!(commiter.verify(c, o).unwrap(), true);
+    }
+
+    #[test]
+    fn test_elgamal_mult() {
         println!("Hello, let's try this El-Gamal commit!");
         let sec = 32;
         println!("I'm gonna use {} bits security", sec);
@@ -55,6 +76,20 @@ mod tests {
         print!("The commit for {} is: ", msg);
         let (c, o) = commiter.commit(msg).unwrap();
         println!("{:#?}", c);
+        assert_eq!(commiter.verify(c, o).unwrap(), true);
+    }
+
+    #[test]
+    fn test_elgamal_ec() {
+        println!("Hello, let's try this El-Gamal commit!");
+        let sec = 32;
+        println!("I'm gonna use {} bits security", sec);
+        let mut commiter = ElGamalCommitterEc::new(sec).unwrap();
+        //println!("{:#?}", commiter);
+        let msg = BigNum::from_u32(456).unwrap();
+        //print!("The commit for {} is: ", msg);
+        let (c, o) = commiter.commit(msg).unwrap();
+        //println!("{}", c);
         assert_eq!(commiter.verify(c, o).unwrap(), true);
     }
 }
