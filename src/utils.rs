471ac44d39e83369c36a177cb6cbeab902d5c69a
lrusso96 <russo.1699981@studenti.uniroma1.it>
2020-06-29 10:31:45 +0200

    Utils: new wrappers for rng

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,19 @@
-use openssl::bn::BigNum;
+use openssl::bn::{BigNum, MsbOption};
 use openssl::error::ErrorStack;
 
 /// Generates a random `BigNum` between 1 and limit - 1.
-pub fn gen_random(limit: &BigNum) -> Result<BigNum, ErrorStack> {
+pub fn rand_range(limit: &BigNum) -> Result<BigNum, ErrorStack> {
     let one = BigNum::from_u32(1)?;
     let mut tmp = BigNum::new()?;
     tmp.checked_sub(limit, &one)?;
     let mut r = BigNum::new()?;
     tmp.rand_range(&mut r)?;
     Ok(r)
+}
+
+/// Generates a random `BigNum` between 1 and limit - 1.
+pub fn rand(secpar: i32) -> Result<BigNum, ErrorStack> {
+    let mut big = BigNum::new()?;
+    big.rand(secpar, MsbOption::MAYBE_ZERO, true)?;
+    Ok(big)
 }
