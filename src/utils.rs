80a54e3654110176df48bdce0fb3dc847897c4f8
lrusso96 <russo.1699981@studenti.uniroma1.it>
2020-06-29 14:59:35 +0200

    Random Number Generator: fix doc

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,7 @@
 use openssl::bn::{BigNum, MsbOption};
 use openssl::error::ErrorStack;
 
-/// Generates a random `BigNum` between 1 and limit - 1.
+/// Generates a random `BigNum` between 1 and `limit` - 1.
 pub fn rand_range(limit: &BigNum) -> Result<BigNum, ErrorStack> {
     let one = BigNum::from_u32(1)?;
     let mut tmp = BigNum::new()?;
@@ -11,7 +11,7 @@ pub fn rand_range(limit: &BigNum) -> Result<BigNum, ErrorStack> {
     Ok(r)
 }
 
-/// Generates a random `BigNum` between 1 and limit - 1.
+/// Generates a random `BigNum` with `secpar` bits.
 pub fn rand(secpar: i32) -> Result<BigNum, ErrorStack> {
     let mut big = BigNum::new()?;
     big.rand(secpar, MsbOption::MAYBE_ZERO, true)?;
